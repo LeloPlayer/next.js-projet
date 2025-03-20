29f6e57460f9f13c4e63c587b26e088fbdcc1162
Samuel <samueleliasps4@gmail.com>
2025-03-20 20:47:09 -0300

    Laços de repitiçao com condicional

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-/* Arrow function */
+const array = [2,3,4,5,6]; // Criando array
 
-const sub = (a,b) => {
-    return a-b;
-}
-
-
-console.log(sub)
+array.forEach(item =>{ // para cada item do array (item)
+    if(item % 2 === 0){ // se fr par...
+        console.log(`O numero ${item} é par`); // faça isso..
+    }
+    else{ // se nao for...
+        console.log(`O numero ${item} é impar`); // faça isso..
+    }
+});
